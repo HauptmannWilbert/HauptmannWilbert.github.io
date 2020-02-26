31ba5b5b2b74b83824c8310f60028d39f36348f9
HauptmannWilbert <theredkss@gmail.com>
2020-02-26 05:26:08 +0300

    Update Second

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-$(document).ready(function(){
-
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-
-    var img = document.createElement("img");
-    img.src="banner.png";
-
-    img.onload= function(){
-        drawText();
-      }
-
-    function drawText() {
-        ctx.font = "bold 100px Open Sans";
-        ctx.fillStyle = ctx.createPattern(img, 'repeat');
-        ctx.textAlign = 'center';
-        var x = canvas.width / 2;
-        ctx.fillText("WATER", x, 400);
-        ctx.fill();
-    }  
-
+$(window).scroll(function() {
+    var st = $(this).scrollTop();
+    $('.banner__img').css({
+        'transform' : 'translate(0%, -' + st /3 + 'px'
+    });
+    $('text').css({
+        'transform' : 'translate(0%, ' + st /3 + 'px'
+    });
 });
