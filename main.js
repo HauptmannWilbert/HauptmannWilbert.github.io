e475f2609364669859ffa43484a8792650afedbe
HauptmannWilbert <theredkss@gmail.com>
2020-02-26 05:50:16 +0300

    Next commit

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 
 $(window).scroll(function() {
     var st = $(this).scrollTop();
+    var width = $("html, body").outerWidth(true);
     $('.banner__img').css({
         'transform' : 'translate(0%, -' + st /3 + 'px'
     });
     $('text').css({
         'transform' : 'translate(0%, ' + st /3 + 'px'
     });
+    if(width <= 767){
+        $('.banner__img').css({
+            'transform' : 'translate(0%, -' + st /0.6 + 'px'
+        });
+        $('text').css({
+            'transform' : 'translate(0%, ' + st /0.6 + 'px'
+        }); 
+    }
 });
