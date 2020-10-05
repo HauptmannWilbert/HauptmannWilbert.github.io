8fbcc2d5a4956e44d785ee3efbc29ce3f44fa397
HauptmannWilbert <theredkss@gmail.com>
2020-10-05 16:14:37 +0300

    add
    pluscommit

--- iboom/main.js
+++ iboom/main.js
@@ -3,12 +3,24 @@
 
 $(function() {
 
+
 $('#big').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   asNavFor: '#thumbs',
   vertical: true,
   arrows: false,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: false
+      }
+    }
+  ]
 });
 
 $('#thumbs').slick({
@@ -19,6 +31,25 @@ $('#thumbs').slick({
   vertical: true,
   prevArrow: $('.card__prev'),
   nextArrow: $('.card__next'),
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 4,
+        vertical: false
+      }
+    },
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        vertical: false
+      }
+    }
+  ]
 });
+
 
 });
