df29c000c218bc1a6e70edac2412f84d6304f152
HauptmannWilbert <theredkss@gmail.com>
2021-01-26 12:13:25 +0300

    newpage
    add

--- krep-komp/main.js
+++ krep-komp/main.js
@@ -223,3 +223,53 @@ $('.recomend__list').owlCarousel({
         }
     }
   });
+
+
+  
+
+$('#big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '#thumbs',
+    vertical: true,
+    verticalSwiping: true,
+    arrows: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          verticalSwiping: false,
+          vertical: false
+        }
+      }
+    ]
+  });
+  
+  $('#thumbs').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '#big',
+    focusOnSelect: true,
+    vertical: true,
+    verticalSwiping: true,
+    prevArrow: $('.card__prev'),
+    nextArrow: $('.card__next'),
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 3,
+          verticalSwiping: false,
+          vertical: false,
+          dots:true,
+          appendDots:$('.card__dots')
+        }
+      }
+    ]
+  });
+  
+  
