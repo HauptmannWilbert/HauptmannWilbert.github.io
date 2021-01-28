d6410e80b31b9cc82014a1a82c8514288cb03e53
HauptmannWilbert <theredkss@gmail.com>
2021-01-28 12:28:29 +0300

    contacts-page
    add

--- krep-komp/main.js
+++ krep-komp/main.js
@@ -273,3 +273,26 @@ $('#big').slick({
   });
   
   
+
+
+  ymaps.ready(init);
+
+  function init () {
+      var myMap = new ymaps.Map("map-1", {
+              center: [55.669494, 37.624530],
+              zoom: 16,
+              controls: ['zoomControl']
+          });
+  
+      myPlacemark = new ymaps.Placemark([55.669518, 37.630472], {
+  
+      }, {
+              iconLayout: 'default#image',
+              iconImageHref: './images/pointer-map.svg',
+              iconImageSize: [40, 40],
+              iconImageOffset: [ -25, -30]
+          });
+  
+   myMap.geoObjects.add(myPlacemark);
+  
+  }
