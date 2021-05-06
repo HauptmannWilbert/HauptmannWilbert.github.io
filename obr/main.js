3cbaf54c6b0ba9c864f14a2895e20a9e0fff49db
HauptmannWilbert <theredkss@gmail.com>
2021-05-06 13:29:17 +0300

    newjsplus
    newjsplus

--- obr/main.js
+++ obr/main.js
@@ -59,6 +59,123 @@ $('.sidebar__close').on('click', function(){
 
 
 
+
+$('.slider__list').owlCarousel({
+  animateIn: 'fadeIn',
+  animateOut: 'fadeOut',
+  margin:20,
+  nav:true,
+  dots:true,
+  dotsContainer:'.slider__dots',
+  navContainer:'.slider__navigation',
+  navText: ['<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26.213 16L19 24l7.213 8L28 30.018 22.574 24 28 17.982 26.213 16z"/></svg>','<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.787 16L28 24l-7.213 8L19 30.018 24.426 24 19 17.982 20.787 16z"/></svg>'],
+  loop:true,
+  autoWidth: false,
+  autoplay:false,
+  autoplayTimeout:5000, 
+  responsive:{ 
+      0:{
+          items:1,
+      },
+      767:{
+          items:1
+      },
+      1024:{
+          items:1
+      },
+      1280:{
+          items:1
+      }
+  }
+});
+
+$('.programms__list').owlCarousel({
+  animateIn: 'fadeIn',
+  animateOut: 'fadeOut',
+  margin: 5,
+  nav:true,
+  dots:false,
+  navContainer:'.programms__navigation',
+  navText: ['<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26.213 16L19 24l7.213 8L28 30.018 22.574 24 28 17.982 26.213 16z"/></svg>','<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.787 16L28 24l-7.213 8L19 30.018 24.426 24 19 17.982 20.787 16z"/></svg>'],
+  loop:true,
+  autoWidth: false,
+  autoplay:false,
+  autoplayTimeout:5000, 
+  responsive:{ 
+      0:{
+          items:1,
+      },
+      767:{
+          items:2
+      },
+      1024:{
+          items:3
+      },
+      1380:{
+          items:4
+      }
+  }
+});
+
+
+$('.projects__list').owlCarousel({
+  animateIn: 'fadeIn',
+  animateOut: 'fadeOut',
+  margin:5,
+  nav:true,
+  dots:false,
+  navContainer:'.projects__navigation',
+  navText: ['<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26.213 16L19 24l7.213 8L28 30.018 22.574 24 28 17.982 26.213 16z"/></svg>','<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.787 16L28 24l-7.213 8L19 30.018 24.426 24 19 17.982 20.787 16z"/></svg>'],
+  loop:true,
+  autoWidth: false,
+  autoplay:false,
+  autoplayTimeout:5000, 
+  responsive:{ 
+      0:{
+          items:1,
+      },
+      767:{
+          items:2
+      },
+      1024:{
+          items:3
+      },
+      1480:{
+          items:4
+      }
+  }
+});
+
+$('.news__list').owlCarousel({
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    margin:30,
+    nav:true,
+    dots:false,
+    navContainer:'.news__navigation',
+    navText: ['<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M26.213 16L19 24l7.213 8L28 30.018 22.574 24 28 17.982 26.213 16z"/></svg>','<svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.787 16L28 24l-7.213 8L19 30.018 24.426 24 19 17.982 20.787 16z"/></svg>'],
+    loop:true,
+    autoWidth: false,
+    autoplay:false,
+    autoplayTimeout:5000, 
+    responsive:{ 
+        0:{
+            items:1,
+        },
+        767:{
+            items:2
+        },
+        1024:{
+            items:3
+        },
+        1480:{
+            items:4
+        }
+    }
+  });
+
+
+
   ymaps.ready(init);
 
   function init () {
